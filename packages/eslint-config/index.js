281bdf73ddc21d95d869daa85b1ba65df5229c75
Roman <roman.lerchster@blue-tomato.com>
2019-05-22 08:31:44 +0200

    Change warnings to errors

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,17 +1,17 @@
 module.exports = {
   extends: 'airbnb-base',
   rules: {
-    "key-spacing": "warn",
-    "object-curly-spacing": ["warn", "always"],
-    "max-len": ["warn", { "code": 120, "tabWidth": 2 }],
-    "operator-linebreak": ["warn", "after"],
+    "key-spacing": "error",
+    "object-curly-spacing": ["error", "always"],
+    "max-len": ["error", { "code": 120, "tabWidth": 2 }],
+    "operator-linebreak": ["error", "after"],
     "import/no-unresolved": "off",
     "no-nested-ternary": "off",
     "no-bitwise": "off",
     "class-methods-use-this": "off",
     "global-require": "off",
     "import/no-dynamic-require": "off",
-    "radix": ["warn", "as-needed"],
-    "arrow-parens": ["warn", "as-needed"]
+    "radix": ["error", "as-needed"],
+    "arrow-parens": ["error", "as-needed"]
   },
 };
