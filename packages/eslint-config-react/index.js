16a261e014a6645cecb8c54b55fbcda1395bcb67
Roman <roman.lerchster@blue-tomato.com>
2019-07-02 10:27:10 +0200

    Added react hooks eslint plugin

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -3,7 +3,15 @@ module.exports = {
     'airbnb',
     '@blue-tomato'
   ],
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+  ],
   rules: {
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
   },
 };
