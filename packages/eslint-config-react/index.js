44c35d5fabb55647821367e7a49eeca6bff3d2be
Roman <roman.lerchster@blue-tomato.com>
2019-08-29 13:53:25 +0200

    New version

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -13,5 +13,6 @@ module.exports = {
     'react/prop-types': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
